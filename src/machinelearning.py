b0853f6e976fb43c3c473028b2bc064a2c00dc3d
Tim <tim.koornstra@gmail.com>
2022-01-17 16:12:11 +0100

    better ML model

--- src/machinelearning.py
+++ src/machinelearning.py
@@ -3,11 +3,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.preprocessing import MinMaxScaler
 
 # TensorFlow Dependencies
 import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Dropout
+
+import math
 
 # Allow GPU memory growth
 physical_devices = tf.config.list_physical_devices("GPU")
@@ -34,10 +37,10 @@ def create_cnn(n_classes):
             input_shape=(129, 2946, 1),
         )
     )
-    model.add(MaxPool2D((2, 2)))
+    #model.add(MaxPool2D((2, 2)))
     model.add(Flatten())
-    model.add(Dense(100, activation="relu", kernel_initializer="he_uniform"))
-    model.add(Dropout(0.5))
+    #model.add(Dense(100, activation="relu", kernel_initializer="he_uniform"))
+    #model.add(Dropout(0.5))
     model.add(Dense(n_classes, activation="softmax"))
 
     print(model.summary())
@@ -48,7 +51,6 @@ def create_cnn(n_classes):
 def create_split(path):
     df = load_data(path)
     df["genre"] = pd.factorize(df["genre"])[0]
-    print(df)
 
     sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=0)
 
@@ -62,20 +64,24 @@ def create_split(path):
 def train_model(model, X, y):
     # Cast it to a numpy array
     X = np.array(X.tolist())
+    X_train = X[:-100]
+    y_train = y[:-100]
+    X_val = X[-100:]
+    y_val = y[-100:]
+    
 
     # Compile model
     model.compile(
         optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
     )
 
-    # Fit data
-    for i in range(0, len(X), fit_step):
-        history = model.fit(
-            x=X[i : i + fit_step],
-            y=y[i : i + fit_step],
-            epochs=1,
-            batch_size=batch_size,
-        )
+    history = model.fit(
+        X_train,
+        y_train,
+        epochs=3,
+        batch_size=batch_size,
+        validation_data=(X_val, y_val)
+    )
 
     return model, history
 
@@ -83,13 +89,12 @@ def train_model(model, X, y):
 def evaluate_model(model, X_test, y_test):
     X_test = np.array(X_test.tolist())
 
-    for i in range(0, len(X_test), eval_step):
-        test_loss, test_acc = model.evaluate(
-            x=X_test[i : i + fit_step],
-            y=y_test[i : i + fit_step],
-            verbose=2,
-            batch_size=batch_size,
-        )
+    _, test_acc = model.evaluate(
+        X_test,
+        y_test,
+        verbose=2,
+        batch_size=batch_size,
+    )
 
     print(test_acc)
 
@@ -97,9 +102,10 @@ def evaluate_model(model, X_test, y_test):
 def plot_accuracy(history):
 
     plt.plot(history.history["accuracy"], label="accuracy")
+    plt.plot(history.history["val_accuracy"], label="val_accuracy")
     plt.xlabel("Epoch")
     plt.ylabel("Accuracy")
-    plt.ylim([0.5, 1])
+    plt.ylim([0.0, 1])
     plt.legend(loc="lower right")
 
     plt.show()
