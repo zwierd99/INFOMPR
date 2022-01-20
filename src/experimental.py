bfdba70311edae4edbc91ee30032bbffe9cc923a
Akkie100 <stephan.akkerman@gmail.com>
2022-01-20 19:00:39 +0100

    Update experimental.py

--- src/experimental.py
+++ src/experimental.py
@@ -1,14 +1,19 @@
-import tensorflow as tf
-import numpy as np
+# Default libraries
 import os
-import matplotlib.pyplot as plt
-import librosa
 import random
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import shutil
+
+# 3rd party libraries
+import numpy as np
+import librosa
+import librosa.display
 import matplotlib
+import matplotlib.pyplot as plt
+
+# Tensorflow libraries
+import tensorflow as tf
 from tensorflow.keras import Sequential, optimizers
-from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, BatchNormalization, Dropout, Activation
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, BatchNormalization, Dropout, Rescaling
 
 # Allow GPU memory growth
 physical_devices = tf.config.list_physical_devices("GPU")
@@ -22,23 +27,35 @@ def make_pics(path):
         
     print(f"Starting mel spectrogram generation in {path}...")
     for subdir, _, files in os.walk(path):
+        # Ignore files that are not folders
         if subdir[len(path) + 1:] != '':
             genre = subdir[len(path) + 1:]
         else:
             continue
         for file in files:
+            # Only process .wav files
             if file.endswith(".wav"):
                 # Check if image already exists
                 if not os.path.exists(f'{path+"_img"}/{genre}/{file.split(".")[0]}.png'):
                     print(file)
+                    
+                    # Get the audio file
                     y,sr = librosa.load(f"{path}/{genre}/{file}",duration=3)
+                    
+                    # Convert it to mel spectrogram
                     mels = librosa.feature.melspectrogram(y=y,sr=sr)
-                    fig = plt.Figure()
-                    FigureCanvas(fig)
-                    plt.imshow(librosa.power_to_db(mels,ref=np.max))
+                    librosa.display.specshow(librosa.power_to_db(mels,ref=np.max))
+                    
+                    # Remove padding
+                    plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0, 
+                                hspace = 0, wspace = 0)
+                    plt.margins(0,0)
+                    
+                    # Make the folder if it doesn't exist
+                    if not os.path.exists(f'{path+"_img"}/{genre}'):
+                        os.makedirs(f'{path+"_img"}/{genre}')
                     plt.savefig(f'{path+"_img"}/{genre}/{file.split(".")[0]}.png')
                     plt.cla()
-
 
 def train_test_split(path):
     
@@ -46,24 +63,30 @@ def train_test_split(path):
         genre = subdir[len(path) + 1:]
         
         random.shuffle(files)
+        
+        # Take 100 files of each genre
         test_files = files[0:100]
         
         for f in test_files:
-            print("Moved file:", f)
+            
+            # Make the folder if it doesn't exist
+            if not os.path.exists(f'{path+"_test"}/{genre}'):
+                os.makedirs(f'{path+"_test"}/{genre}')
             shutil.move(f"{path}/{genre}/{f}",f'{path+"_test"}/{genre}/{f}')
+            
+            print("Moved file:", f)
 
 # https://www.tensorflow.org/tutorials/load_data/images
 def generators(path):
-    img_height=640#288
-    img_width=480#432
-    batch_size=128
+    img_height=640
+    img_width=480
     
-    # For rescaling the data
-    normalization_layer = tf.keras.layers.Rescaling(1./255)
+    # For rescaling the data to values betwen 0-1
+    normalization_layer = Rescaling(1./255)
     
     train_ds = tf.keras.utils.image_dataset_from_directory(
     path,
-    validation_split=0.2,
+    validation_split=0.1,
     subset="training",
     color_mode='rgba',
     seed=123,
@@ -71,21 +94,30 @@ def generators(path):
     batch_size=batch_size).map(lambda x, y: (normalization_layer(x), y))
     
     val_ds = tf.keras.utils.image_dataset_from_directory(
-    path+"_test",
-    validation_split=0.2,
+    path,
+    validation_split=0.1,
     subset="validation",
     color_mode='rgba',
     seed=123,
     image_size=(img_height, img_width),
     batch_size=batch_size).map(lambda x, y: (normalization_layer(x), y))
     
-    return train_ds, val_ds
+    test_ds = tf.keras.utils.image_dataset_from_directory(
+    path+'_test',
+    validation_split=0,
+    color_mode='rgba',
+    image_size=(img_height, img_width),
+    batch_size=batch_size).map(lambda x, y: (normalization_layer(x), y))
+    
+    return train_ds, val_ds, test_ds
 
 def model():
     input_shape = (640, 480, 4)
     n_classes = 10    
     
     model = Sequential()
+    
+    # For rescaling the data   
     model.add(Conv2D(8, (3, 3), activation = "relu", input_shape = input_shape))
     model.add(BatchNormalization(axis=3))
     model.add(MaxPool2D())    
@@ -114,24 +146,36 @@ def model():
 
     return model
 
+
 def train_model(model, train, validation):
     
     # Compile model
     model.compile(
-        optimizer=optimizers.Adam(learning_rate=0.005), loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+        # Maybe try tf.losses.SparseCategoricalCrossentropy(from_logits=True)
+        optimizer=optimizers.Adam(learning_rate=learning_rate), loss="sparse_categorical_crossentropy", metrics=["accuracy"]
     )
 
     history = model.fit(
         train,
-        epochs=70,
+        epochs=epochs,
         validation_data=validation
     )
     
     return model, history
 
+
+def evaluate_model(model, test):
+    
+    _, test_acc = model.evaluate(
+        test,
+    )
+
+    print(f"Test Accuracy: {test_acc}")
+    
+
 def plot_accuracy(hist):
     
-    fig, axs = plt.subplots(2)
+    _, axs = plt.subplots(2)
         
     # accuracy subplot
     axs[0].plot(hist.history["accuracy"], label="Train Accuracy")
@@ -149,14 +193,27 @@ def plot_accuracy(hist):
     axs[1].set_title("Error Eval")
 
     plt.show()
+  
     
+def setup_files(PATH):
+    make_pics(PATH)
+    train_test_split(PATH)
+        
+
 if __name__ == "__main__":
-    PATH = "F:/data_3sec_img"
-    #make_pics(PATH)
-    #train_test_split(PATH)
-    train, test = generators(PATH)
-    cnn = model()
-    trained_model, history = train_model(cnn, train, test)
-    plot_accuracy(history)
     
+    # Global variables
+    PATH = "F:/data_3sec_img"
+    batch_size=32
+    learning_rate=0.005
+    epochs=70
+    
+    # Uncomment if you do not have the images + test files
+    #setup_files(PATH)
+    
+    train, val, test = generators(PATH)
+    cnn = model()
+    trained_model, history = train_model(cnn, train, val)
+    evaluate_model(trained_model, test)
+    plot_accuracy(history)
     
