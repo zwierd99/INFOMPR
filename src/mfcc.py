ca3a54b1c8f3a4c53265954c81a0a0ba301d544a
ndkrebbers <n.d.krebbers@gmail.com>
2022-01-21 13:50:37 +0100

    small fix mel and mfcc

--- src/mfcc.py
+++ src/mfcc.py
@@ -12,7 +12,7 @@ def make_pickle(path):
     Uses the audio files to generate mel spectograms
     """
     
-    df = pd.DataFrame(columns=["genre", "spectrogram", "mfcc"])
+    df = pd.DataFrame(columns=["genre", "spectrogram"])
     
     print(f"Starting mfcc generation in {path}...")
     for subdir, _, files in os.walk(path):
@@ -27,17 +27,50 @@ def make_pickle(path):
                 y,sr = librosa.load(f"{path}/{genre}/{file}",duration=3)
                 mel_spec = librosa.power_to_db(librosa.feature.melspectrogram(y=y,sr=sr),ref=np.max)
 
-                mel_spec_arr = np.asarray(mel_spec).astype("float32")
 
                 mfcc = librosa.feature.mfcc(S=mel_spec)
                 mfcc = np.asarray(mfcc).astype("float32")
                 
-                if mfcc.shape == (20,130) and mel_spec_arr.shape == (128,130):
+                if mfcc.shape == (20,130):
                     df = df.append({"genre" : genre,
-                                    "spectrogram": mel_spec_arr,
-                                    "mfcc": mfcc},
+                                    "spectrogram": mfcc},
                                     ignore_index=True)
 
 
     df.to_pickle(f"{path}/mfcc.pkl")
+    print("Done")
+
+
+def make_combined_pickle(path):
+    """
+    Uses the audio files to generate mel spectograms
+    """
+
+    df = pd.DataFrame(columns=["genre", "spectrogram", "mfcc"])
+
+    print(f"Starting mfcc generation in {path}...")
+    for subdir, _, files in os.walk(path):
+        if subdir[len(path) + 1:] != '':
+            genre = subdir[len(path) + 1:]
+        else:
+            continue
+        for file in files:
+            if file.endswith(".wav"):
+                print(file)
+
+                y, sr = librosa.load(f"{path}/{genre}/{file}", duration=3)
+                mel_spec = librosa.power_to_db(librosa.feature.melspectrogram(y=y, sr=sr), ref=np.max)
+
+                mel_spec_arr = np.asarray(mel_spec).astype("float32")
+
+                mfcc = librosa.feature.mfcc(S=mel_spec)
+                mfcc = np.asarray(mfcc).astype("float32")
+
+                if mfcc.shape == (20, 130) and mel_spec_arr.shape == (128, 130):
+                    df = df.append({"genre": genre,
+                                    "spectrogram": mel_spec_arr,
+                                    "mfcc": mfcc},
+                                   ignore_index=True)
+
+    df.to_pickle(f"{path}/mfcc_and_spectrogram.pkl")
     print("Done")
