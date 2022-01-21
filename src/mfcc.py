7a183e0d512efc3672fb93cfdf57ebcaf06fdf33
ndkrebbers <n.d.krebbers@gmail.com>
2022-01-21 13:29:14 +0100

    added potential mfcc and spectrograms combined
    kan niet runnen want mn python is fucked ofzo??

--- src/mfcc.py
+++ src/mfcc.py
@@ -12,7 +12,7 @@ def make_pickle(path):
     Uses the audio files to generate mel spectograms
     """
     
-    df = pd.DataFrame(columns=["genre", "spectrogram"])
+    df = pd.DataFrame(columns=["genre", "spectrogram", "mfcc"])
     
     print(f"Starting mfcc generation in {path}...")
     for subdir, _, files in os.walk(path):
@@ -26,13 +26,16 @@ def make_pickle(path):
                                 
                 y,sr = librosa.load(f"{path}/{genre}/{file}",duration=3)
                 mel_spec = librosa.power_to_db(librosa.feature.melspectrogram(y=y,sr=sr),ref=np.max)
-                
+
+                mel_spec_arr = np.asarray(mel_spec).astype("float32")
+
                 mfcc = librosa.feature.mfcc(S=mel_spec)
                 mfcc = np.asarray(mfcc).astype("float32")
                 
-                if mfcc.shape == (20,130):
+                if mfcc.shape == (20,130) and mel_spec_arr.shape == (128,130):
                     df = df.append({"genre" : genre,
-                                    "spectrogram": mfcc},
+                                    "spectrogram": mel_spec_arr,
+                                    "mfcc": mfcc},
                                     ignore_index=True)
 
 
